f52df925e641720718ee832835197f16bcee964b
atm-ph <phf@atm-consulting.fr>
2018-10-29 16:32:58 +0100

    Git init

---
+++ script/create-maj-base.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * Script créant et vérifiant que les champs requis s'ajoutent bien
+ */
+
+if(!defined('INC_FROM_DOLIBARR')) {
+	define('INC_FROM_CRON_SCRIPT', true);
+
+	require('../config.php');
+} else {
+	global $db;
+}
+
+
+/* uncomment
+
+
+dol_include_once('/brutepass/class/brutepass.class.php');
+
+$o=new brutepass($db);
+$o->init_db_by_vars();
+*/
